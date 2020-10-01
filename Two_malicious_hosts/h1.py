99c159ebcab47cc8bcc596bb4d17cc19c7bcac18
ce441-981-95101666 <97.amirsabzi@gmail.com>
2020-10-01 17:17:58 +0330

    e2

---
+++ Two_malicious_hosts/h1.py
@@ -0,0 +1,52 @@
+import sys
+import socket
+import random
+import struct
+import time
+import datetime
+from subprocess import Popen, PIPE
+from scapy.all import *
+import shlex
+from variables import *
+
+
+def main():
+    print "waiting"
+    file = open("/home/sdn/covert_channel/h1_log.txt", "w")
+    while True:
+        if datetime.datetime.now().strftime('%S') == '00':
+            break
+    print "running"
+    timeout = 0.1
+    cmd = "timeout 0.01 ping 10.0.0.2 -c 1"
+    k = 0
+    intervals = []
+    for i in range(message_length):
+        start_time=time.time()
+        print "Round-" + str(k) + " is started at " + datetime.datetime.now().strftime('%H:%M:%S:%f')
+        file.write("Round-" + str(k) + " is started at: " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        k = k + 1
+        time.sleep(delta_s + delta_r)
+        phase3_start = time.time()
+        file.write("---" + "(P3)tried to redefine flows on the switch at: " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        output = Popen(cmd,stdout=PIPE,shell=True)
+        response = output.communicate()[0]
+        file.write("---" + "(P3)flow reconfiguration considered DONE! at: " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        phase3_finish = time.time()
+        phase3_delay = phase3_finish - phase3_start
+        file.write("---" + "(P3)Phase-3 delay is: " + str(phase3_delay) + "\n")
+        print phase3_delay
+        time.sleep(delta_p - phase3_delay)
+        stop_time=time.time()
+        time_difference = stop_time - start_time
+        intervals.append(time_difference)
+        file.write("---" + "Round-" + str(k) + " is finished at: " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        file.write("---" + "Round-" + str(k) + " taken time: " + str(time_difference) + "\n")
+    avg_round_time = sum(intervals) / len(intervals)
+    file.write("---------------------------------\n")
+    file.write("Average round duration is: " + str(avg_round_time) +  "\n")
+    file.close()
+if __name__ == '__main__':
+    main()
+
+
---
+++ Two_malicious_hosts/h2.py
@@ -0,0 +1,73 @@
+import sys
+import socket
+import random
+import struct
+import time
+from subprocess import Popen, PIPE
+from scapy.all import *
+import shlex
+import datetime
+from variables import *
+
+
+def main():
+    print "Calculatiing Timeout"
+    #output = Popen("ping 10.0.0.1 -c 10",stdout=PIPE,shell=True)
+    #string = output.communicate()[0]
+    #splitted = string.split('/')
+    #timeout = 0.001 * float(splitted[4]) * 40
+    timeout = 0.02
+    print "Timeout is: " + str(timeout)
+    print "waiting"
+    while True:
+        if datetime.datetime.now().strftime('%S') == '00':
+            break
+    print "running"
+    recv_array = []
+    cmd = "timeout " + str(timeout) + " ping 10.0.0.1 -c 1 || echo Failed"
+    k = 0
+    intervals=[]
+    file = open("/home/sdn/covert_channel/h2_log.txt", "w")
+    for i in range(message_length):
+        start_time=time.time()
+        print "Round" + str(k) + " is started at " + datetime.datetime.now().strftime('%H:%M:%S')
+        file.write("Round" + str(k) + " is started at: " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        k = k + 1
+        time.sleep(delta_s)
+        phase2_start = time.time()
+        file.write("---" + "(P2)Tried to check received bit at: " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        output = Popen(cmd,stdout=PIPE,shell=True)
+        response = output.communicate()[0]
+        if response == 'Failed\n':
+            recv_array.append('1')
+            file.write("---" + "The received bit detected and it is \"1\": " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        else:
+            recv_array.append('0')
+            file.write("---" + "The received bit detected and it is \"0\": " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        phase2_finish = time.time()
+        phase2_delay = phase2_finish - phase2_start
+        print phase2_delay
+        time.sleep(delta_r - phase2_delay + delta_p)
+        stop_time = time.time()
+        time_difference = stop_time - start_time
+        intervals.append(time_difference)
+        file.write("---" + "Round" + str(k) + "is finished at: " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
+        file.write("---" + "Round" + str(k) + "taken time: " + str(time_difference) + "\n")
+    avg_round_time = sum(intervals) / len(intervals)
+    file.write("---------------------------------\n")
+    file.write("Average round duration is: " + str(avg_round_time)+ "\n")
+    file.close()
+    print "Received Bitstring"
+    print recv_array
+    print "Sended Bitstring"
+    print binary_array
+    counter = 0
+    for i in range(message_length):
+        if recv_array[i] != binary_array[i]:
+            counter = counter + 1
+    error_ratio = float(counter)/float(message_length)
+    print "Error ratio is: " + str(error_ratio)
+
+if __name__ == '__main__':
+    main()
+                                                                                                                              
