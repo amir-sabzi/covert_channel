df4fee806beaa731fda99b949890344247259aa9
ce441-981-95101666 <97.amirsabzi@gmail.com>
2020-10-03 18:24:45 +0330

    new test

--- Two_malicious_hosts/h3_sender.py
+++ Two_malicious_hosts/h3_sender.py
@@ -31,8 +31,8 @@ def main():
         phase1_finish=time.time()
         phase1_delay = phase1_finish - phase1_start
         print phase1_delay
-        time.sleep(delta_2 - phase1_delay)
-        time.sleep(delta_1)
+        time.sleep(delta_1 - phase1_delay)
+        time.sleep(delta_2)
         i = i + 1
     log.close()
 if __name__ == '__main__':
