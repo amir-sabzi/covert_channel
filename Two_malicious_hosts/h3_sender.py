fd167a81e85e78a2d59943c7f336e10a1594ca36
ce441-981-95101666 <97.amirsabzi@gmail.com>
2020-10-03 19:33:12 +0330

    new test

--- Two_malicious_hosts/h3_sender.py
+++ Two_malicious_hosts/h3_sender.py
@@ -16,7 +16,7 @@ def main():
         if datetime.datetime.now().strftime('%S') == '00':
             break
     print "running"
-    cmd = "timeout 0.04 nping --source-mac 00:00:00:00:00:01 -S 10.0.0.1 10.0.0.2 -c 1"
+    cmd = "timeout 0.04 nping --source-mac 00:00:00:00:00:01 10.0.0.2 -c 1"
     intervals=[]
     i = 0
     log = open("/home/amirs97/covert_channel/Two_malicious_hosts/sender_log.txt", "w")
@@ -26,7 +26,7 @@ def main():
         phase1_start=time.time()
         #print "Round" + str(i) +  " is started at " + datetime.datetime.now().strftime('%H:%M:%S')
         #output = Popen(cmd,stdout=PIPE,shell=True)
-        if True:
+        if element == '1':
             output = Popen(cmd,stdout=PIPE,shell=True)
             #srp(Ether(dst="ff:ff:ff:ff:ff:ff" , src="00:00:00:00:00:01")/ARP(pdst="10.0.0.2"),timeout=0)
         phase1_finish=time.time()
