c29cd0f6cf6ea543097ced37a10ee960634a961d
ce441-981-95101666 <97.amirsabzi@gmail.com>
2020-10-03 16:02:59 +0330

    new test

--- Two_malicious_hosts/h3_test2.py
+++ Two_malicious_hosts/h3_test2.py
@@ -16,14 +16,13 @@ def main():
         if datetime.datetime.now().strftime('%S') == '00':
             break
     print "running"
-    cmd = "timeout 0.4 nping --source-mac 00:00:00:00:00:01 -S 10.0.0.1 10.0.0.2 -c 1"
+    cmd = "timeout 0.04 nping --source-mac 00:00:00:00:00:01 -S 10.0.0.1 10.0.0.2 -c 1"
     intervals=[]
     for i in range(sample_num):
         phase1_start=time.time()
         #print "Round" + str(i) +  " is started at " + datetime.datetime.now().strftime('%H:%M:%S')
-        #output = Popen(cmd,stdout=PIPE,shell=True)
-        #response = output.communicate()[0]
-        ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff" , src="00:00:00:00:00:01")/ARP(pdst="10.0.0.2"),timeout=0.04)
+        output = Popen(cmd,stdout=PIPE,shell=True)
+        #ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff" , src="00:00:00:00:00:01")/ARP(pdst="10.0.0.2"),timeout=0.04)
         phase1_finish=time.time()
         phase1_delay = phase1_finish - phase1_start
         print phase1_delay
