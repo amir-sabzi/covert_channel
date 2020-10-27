5fb477c7ee39c83bb0b65b06faed282fe79bc5a3
ce441-981-95101666 <97.amirsabzi@gmail.com>
2020-10-27 22:00:17 +0330

    frame

--- Two_malicious_hosts/variables.py
+++ Two_malicious_hosts/variables.py
@@ -7,7 +7,7 @@ message_length = (byte_num + 1) * 8
 sent_message = "gPet"
 b = [ord(i) for i in sent_message] 
 c = ['1' + '{0:07b}'.format(i) for i in b]
-c.append('00000000')
+c.append('10000000')
 d = [list(i) for i in c]
 send_array = [item for sublist in d for item in sublist]
 #send_array = ['1', '0', '0', '1', '0', '1', '1', '1', '0', '1', '0', '0', '1', '1', '0', '1', '0', '1', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '1', '0', '1', '1', '0', '0', '1', '0', '0', '1', '0', '0', '0', '1', '1', '0', '1']
