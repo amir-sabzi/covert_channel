6e8f6c682ee7b011a95ad565f2c46d75f5b02d3e
ce441-981-95101666 <97.amirsabzi@gmail.com>
2020-10-27 17:45:32 +0330

    frame

---
+++ Two_malicious_hosts/local_test.py
@@ -0,0 +1,14 @@
+import random
+import string
+length = 2
+a = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+print a 
+b = [ord(i) for i in a] 
+print b
+c = ['1' + '{0:07b}'.format(i) for i in b]
+c.append('00000000')
+print c
+d = [list(i) for i in c]
+print d
+flat_list = [item for sublist in d for item in sublist]
+print flat_list
--- Two_malicious_hosts/variables.py
+++ Two_malicious_hosts/variables.py
@@ -1,5 +1,14 @@
+import random
+import string 
 delta_1 = 0.07
 delta_2 = 0.03
-message_length = 50
-send_array = ['1', '0', '0', '1', '0', '1', '1', '1', '0', '1', '0', '0', '1', '1', '0', '1', '0', '1', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '1', '0', '1', '1', '0', '0', '1', '0', '0', '1', '0', '0', '0', '1', '1', '0', '1']
+byte_num = 128
+message_length = (byte_num + 1) * 8
+a = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(byte_num))
+b = [ord(i) for i in a] 
+c = ['1' + '{0:07b}'.format(i) for i in b]
+c.append('00000000')
+d = [list(i) for i in c]
+send_array = [item for sublist in d for item in sublist]
+#send_array = ['1', '0', '0', '1', '0', '1', '1', '1', '0', '1', '0', '0', '1', '1', '0', '1', '0', '1', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '1', '0', '1', '1', '0', '0', '1', '0', '0', '1', '0', '0', '0', '1', '1', '0', '1']
 #send_array = ['0', '1', '0', '1', '0']
