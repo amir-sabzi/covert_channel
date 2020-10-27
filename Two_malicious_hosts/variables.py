fe8b05ae4f939b597eac0e92a2dc2092215c732f
ce441-981-95101666 <97.amirsabzi@gmail.com>
2020-10-27 18:33:32 +0330

    frame

--- Two_malicious_hosts/variables.py
+++ Two_malicious_hosts/variables.py
@@ -2,7 +2,7 @@ import random
 import string 
 delta_1 = 0.08
 delta_2 = 0.045
-byte_num = 50
+byte_num = 128
 message_length = (byte_num + 1) * 8
 a = "gPet4gD4hDjuTKkAP25iKLDBjHefd2zk0qtksQcd9RAiDhe5ism7zj8jGNLN86i6z58oXaMnOzXn42NS7r8H0Pjf3KAF2s04bMJcjK9PH7QahULBbwD9c8fZCpwlwtwg"
 b = [ord(i) for i in a] 
