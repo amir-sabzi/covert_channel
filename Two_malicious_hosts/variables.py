2e2c6c7ef1cf5b78a2bd7365f32bac51cabc7cf4
ce441-981-95101666 <97.amirsabzi@gmail.com>
2021-03-27 14:22:40 +0430

    e3

--- Two_malicious_hosts/variables.py
+++ Two_malicious_hosts/variables.py
@@ -2,9 +2,10 @@ import random
 import string 
 delta_1 = 0.08
 delta_2 = 0.045
-byte_num = 16
+byte_num = 4
 message_length = (byte_num + 1) * 8
-sent_message = "7bU4ETM9cciphU280pVqr5UFpeRE8BZpEemmSWzEBMzi3Bxt4P9rtQfIdPhlHCNI1CbHV2bey75NK6sZ8MXDgbI7DR6zQF6vHulUPYp2T98Nn7ur99EALdALozDEMKQP"
+#sent_message = "7bU4ETM9cciphU280pVqr5UFpeRE8BZpEemmSWzEBMzi3Bxt4P9rtQfIdPhlHCNI1CbHV2bey75NK6sZ8MXDgbI7DR6zQF6vHulUPYp2T98Nn7ur99EALdALozDEMKQP"
+sent_message = "7bU4"
 b = [ord(i) for i in sent_message] 
 c = ['1' + '{0:07b}'.format(i) for i in b]
 c.append('10000000')
