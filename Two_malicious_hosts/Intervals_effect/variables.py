4bd031e7921b910ad21c422a5cbf87401e0ac252
ce441-981-95101666 <97.amirsabzi@gmail.com>
2021-06-22 12:07:35 +0430

    e3

--- Two_malicious_hosts/Intervals_effect/variables.py
+++ Two_malicious_hosts/Intervals_effect/variables.py
@@ -3,8 +3,8 @@ import string
 import numpy as np 
 
 
-delta_1_array = np.linspace(0.01,2,10)
-delta_2_array = np.linspace(0.01,1,10)
+delta_1_array = np.linspace(1,2,1)
+delta_2_array = np.linspace(1,1,1)
 
 #delta_1 = 0.09
 #delta_2 = 0.06
@@ -14,7 +14,7 @@ number_of_tests = 32
 calibration_delta_1 = 0.3
 calibration_delta_2 = 0.3
 byte_num = 4
-callibration_array_size = 512
+callibration_array_size = 128
 callibration_array = ['1','0'] * int(callibration_array_size/2)
 message_length = (byte_num + 1) * 8
 #sent_message = "7bU4ETM9cciphU280pVqr5UFpeRE8BZpEemmSWzEBMzi3Bxt4P9rtQfIdPhlHCNI1CbHV2bey75NK6sZ8MXDgbI7DR6zQF6vHulUPYp2T98Nn7ur99EALdALozDEMKQP"
@@ -24,6 +24,5 @@ c = ['1' + '{0:07b}'.format(i) for i in b]
 c.append('10000000')
 d = [list(i) for i in c]
 send_array = [item for sublist in d for item in sublist]
-
 
 
