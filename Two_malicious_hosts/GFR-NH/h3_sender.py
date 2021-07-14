8afc8cec932683f17eeda313ef8d8d10b59dedb4
ce441-981-95101666 <97.amirsabzi@gmail.com>
2021-07-15 00:39:12 +0430

    e3

--- Two_malicious_hosts/GFR-NH/h3_sender.py
+++ Two_malicious_hosts/GFR-NH/h3_sender.py
@@ -17,18 +17,15 @@ def sender(sending_thread_array,source_mac,interface_name):
             break
     print "running"
     #cmd = "timeout 0.05 nping --source-mac 00:00:00:00:00:01 10.0.0.2 -c 1"
-    cmd_timeout = calibration_delta_1 * 2/5
-    cmd = "timeout " + str(cmd_timeout) + " nping " + " --interface " + interface_name + " --source-mac " + source_mac + " 10.0.0.2 -c 1"
     log = open("/home/amirs97/covert_channel/Two_malicious_hosts/calibration/sender_calibration_log_" + interface_name + ".txt", "w")
     i = 1
     for element in sending_thread_array:
         calibration_phase1_start = time.time()
+        log.write("Round" + str(i) +  " is started at " + datetime.datetime.now().strftime('%H:%M:%S:%f') + "\n")
         if element == '1':
-            #srp(Ether(dst="ff:ff:ff:ff:ff:ff" , src=source_mac)/ARP(pdst="10.0.0.2"), timeout=cmd_timeout, iface=interface_name,verbose=0)
-            output = Popen(cmd,stdout=PIPE,shell=True)
+            srp(Ether(dst="ff:ff:ff:ff:ff:ff" , src=source_mac)/ARP(pdst="10.0.0.2"),timeout=0.05,iface=interface_name,verbose=0)
         calibration_phase1_finish = time.time()
         calibration_phase1_delay = calibration_phase1_finish - calibration_phase1_start
-        print(calibration_phase1_delay)
         time.sleep(calibration_delta_1 - calibration_phase1_delay)
         time.sleep(calibration_delta_2)
         i += 1
